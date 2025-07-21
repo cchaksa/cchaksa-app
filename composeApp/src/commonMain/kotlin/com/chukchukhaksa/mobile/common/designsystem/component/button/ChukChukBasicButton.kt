eb7ba6565f1be1ca33963591903ce37a19a22083
BEEEAM-J <cbj0545@naver.com>
2025-07-21 23:07:23 +0900

    feat: 공용 컴포넌트 추가
    - `ChukChukAppBarWithTitle`: 타이틀과 뒤로가기 버튼이 있는 앱 바
    - `ChukChukBasicButton`: 기본적인 형태의 버튼
    - `ChukChukSelectionContainer`: 선택 가능한 컨테이너

---
+++ composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/common/designsystem/component/button/ChukChukBasicButton.kt
@@ -0,0 +1,51 @@
+package com.chukchukhaksa.mobile.common.designsystem.component.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ChukChukBasicButton(
+  modifier: Modifier = Modifier,
+  text: String,
+  enable: Boolean,
+  onClick: () -> Unit,
+) {
+  val (containerBackground, textColor) = if(enable) {
+    Pair(Color(0xff2B2B2B), Color(0xffFFFFFF))
+  } else {
+    Pair(Color(0xffD9D9E3), Color(0xffA3A2B1))
+  }
+  Box(
+    modifier = modifier
+      .clip(RoundedCornerShape(10.dp))
+      .background(containerBackground)
+      .clickable { onClick() }
+      .padding(18.dp),
+  ) {
+      Text(
+        modifier = Modifier.fillMaxSize(),
+        text = text,
+        color = textColor,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        style = TextStyle(letterSpacing = (-0.02).em),
+      )
+  }
+}
---
+++ composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/common/designsystem/component/container/ChukChukSelectionContainer.kt
@@ -0,0 +1,49 @@
+package com.chukchukhaksa.mobile.common.designsystem.component.container
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ChukChukSelectionContainer(
+  modifier: Modifier = Modifier,
+  text: String,
+  isSelected: Boolean,
+) {
+  val (containerBackground, textColor, borderColor) = if (isSelected) {
+    Triple(Color(0xff2B2B2B), Color(0xff9F94FF), Color(0xff2B2B2B))
+  } else {
+    Triple(Color(0xffFFFFFF), Color(0xff2B2B2B), Color(0xFFE8E9EF))
+  }
+  Box(
+    modifier = modifier
+      .clip(RoundedCornerShape(16.dp))
+      .background(containerBackground)
+      .border(1.dp, borderColor, RoundedCornerShape(16.dp))
+      .padding(vertical = 21.dp, horizontal = 18.dp),
+  ) {
+    Text(
+      modifier = Modifier.fillMaxSize(),
+      text = text,
+      color = textColor,
+      fontSize = 18.sp,
+      textAlign = TextAlign.Start,
+      fontWeight = FontWeight.Bold,
+      style = TextStyle(letterSpacing = (-0.02).em),
+    )
+  }
+}
