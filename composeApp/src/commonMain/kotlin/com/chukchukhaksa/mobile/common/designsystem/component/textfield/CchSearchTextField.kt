6d904373a19ffeed4f09612df5dfc6e15885ddf6
jinukeu <koownij@kakao.com>
2025-07-27 19:28:07 +0900

    feat: 검색 TextField UI 개선
    - `CchSearchTextField`의 검색 아이콘과 텍스트 필드 사이에 간격 추가
    - 미리보기 `TextField.kt`의 `afterValue` 텍스트 길이 증가

--- composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/common/designsystem/component/textfield/CchSearchTextField.kt
+++ composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/common/designsystem/component/textfield/CchSearchTextField.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -149,6 +151,7 @@ fun CchSearchTextField(
           }
           innerTextField()
         }
+        Spacer(modifier = Modifier.width(5.dp))
         Icon(
           painter = painterResource(Res.drawable.ic_search),
           contentDescription = null,
