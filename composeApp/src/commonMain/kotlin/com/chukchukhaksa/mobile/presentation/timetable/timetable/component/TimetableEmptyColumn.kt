92d563776561f7b933b04b42f452c07332e0c911
BEEEAM-J <cbj0545@naver.com>
2025-08-02 15:41:08 +0900

    feat: 시간표 없을 때 UI 수정
    - 시간표가 없을 때 보여지는 화면의 UI를 수정했습니다.
    - 로고 이미지를 추가하고, 텍스트 스타일 및 레이아웃을 조정하여 사용자 경험을 개선했습니다.
    - `TimetableEmptyColumn` 컴포저블 함수 내부에 로고 이미지와 버튼 스타일을 변경했습니다.

--- composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/presentation/timetable/timetable/component/TimetableEmptyColumn.kt
+++ composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/presentation/timetable/timetable/component/TimetableEmptyColumn.kt
@@ -1,9 +1,13 @@
 package com.chukchukhaksa.mobile.presentation.timetable.timetable.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,11 +15,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import chukchukhaksa.composeapp.generated.resources.Res
+import chukchukhaksa.composeapp.generated.resources.ic_cch_logo_ios
 import chukchukhaksa.composeapp.generated.resources.timetable_screen_create_timetable
 import chukchukhaksa.composeapp.generated.resources.timetable_screen_create_timetable_button
+import com.chukchukhaksa.mobile.common.designsystem.component.button.CchBasicButton
 import com.chukchukhaksa.mobile.common.designsystem.component.button.SuwikiContainedMediumButton
+import com.chukchukhaksa.mobile.common.designsystem.theme.CchTheme
+import com.chukchukhaksa.mobile.common.designsystem.theme.Gray200
+import com.chukchukhaksa.mobile.common.designsystem.theme.Gray600
 import com.chukchukhaksa.mobile.common.designsystem.theme.Gray95
 import com.chukchukhaksa.mobile.common.designsystem.theme.SuwikiTheme
+import com.chukchukhaksa.mobile.presentation.timetable.timetable.component.timetable.timetableBorderWidth
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -24,23 +35,35 @@ fun TimetableEmptyColumn(
   onClickAdd: () -> Unit = {},
 ) {
   Column(
-    modifier = modifier,
+    modifier = modifier
+      .padding(top = 8.dp, bottom = 2.dp, start = 20.dp, end = 20.dp)
+      .fillMaxSize()
+      .border(width = timetableBorderWidth, color = Gray200, shape = RoundedCornerShape(12.dp))
+      .padding(horizontal = 73.dp),
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
-    Spacer(modifier = Modifier.padding(top = 130.dp))
+    Image(
+      modifier = Modifier
+//        .size(100.dp)
+        .padding(top = 190.dp, bottom = 14.dp),
+      painter = painterResource(Res.drawable.ic_cch_logo_ios),
+      contentDescription = "cch logo",
+    )
 
     Text(
-      text = stringResource(Res.string.timetable_screen_create_timetable),
-      color = Gray95,
-      style = SuwikiTheme.typography.header4,
+      modifier = Modifier.padding(bottom = 40.dp),
+      text = "아직 만들어진 시간표가 없어요",
+      color = Gray600,
+      style = CchTheme.typography.bodyMd,
       textAlign = TextAlign.Center,
     )
 
-    Spacer(modifier = Modifier.size(28.dp))
-
-    SuwikiContainedMediumButton(
-      text = stringResource(Res.string.timetable_screen_create_timetable_button),
-      onClick = onClickAdd,
+    CchBasicButton(
+      modifier = Modifier.padding(horizontal = 20.dp),
+      text = "시간표 생성하기",
+      enable = true,
+      textStyle = CchTheme.typography.bodyMdStrong,
+      onClick = { onClickAdd() }
     )
   }
 }
