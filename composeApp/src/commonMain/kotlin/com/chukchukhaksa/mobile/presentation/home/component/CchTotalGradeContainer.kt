ccba1ff260de90211d564ca36251aa96b3f2b76b
BEEEAM-J <cbj0545@naver.com>
2026-03-04 23:20:21 +0900

    feat: Total Grade Container 추가
    - CchTotalGradeContainer, CchTotalGradeContainerPreview 추가

---
+++ composeApp/src/commonMain/kotlin/com/chukchukhaksa/mobile/presentation/home/component/CchTotalGradeContainer.kt
@@ -0,0 +1,86 @@
+package com.chukchukhaksa.mobile.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.chukchukhaksa.mobile.common.designsystem.theme.Black100
+import com.chukchukhaksa.mobile.common.designsystem.theme.CchTheme
+import com.chukchukhaksa.mobile.common.designsystem.theme.Purple500
+import com.chukchukhaksa.mobile.common.designsystem.theme.White100
+
+@Composable
+fun CchTotalGradeContainer(
+  totalEarnedCredits: Int,
+  cumulativeGpa: Double,
+  percentile: Double,
+) {
+  Row(
+    modifier = Modifier
+      .clip(RoundedCornerShape(16.dp))
+      .background(Black100)
+      .padding(vertical = 16.dp, horizontal = 29.dp)
+      .wrapContentHeight(),
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.spacedBy(32.dp)
+  ) {
+    CchTotalGradeContainerItem(
+      title = "취득학점",
+      value = totalEarnedCredits.toString()
+    )
+    CchTotalGradeContainerDivider()
+    CchTotalGradeContainerItem(
+      title = "평점 평균",
+      value = cumulativeGpa.toString()
+    )
+    CchTotalGradeContainerDivider()
+    CchTotalGradeContainerItem(
+      title = "백분위",
+      value = percentile.toString()
+    )
+  }
+}
+
+@Composable
+fun CchTotalGradeContainerItem(
+  title: String,
+  value: String,
+) {
+  Column(
+    modifier = Modifier.fillMaxHeight(),
+    verticalArrangement = Arrangement.spacedBy(4.dp),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Text(
+      text = title,
+      color = White100,
+      style = CchTheme.typography.bodySm
+    )
+    Text(
+      text = value,
+      color = Purple500,
+      style = CchTheme.typography.bodyExlg
+    )
+  }
+}
+
+@Composable
+fun CchTotalGradeContainerDivider() {
+  VerticalDivider(
+    modifier = Modifier.padding(vertical = 17.dp),
+    thickness = 1.dp,
+    color = Color(0xFF505050)
+  )
+}
